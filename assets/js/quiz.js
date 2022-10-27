d6ddbeba6dd6b007e211ba3856efaabb444887d1
tiiingaling <wongwong02@live.co.uk>
2022-10-27 14:26:04 +0100

    start and end game functions

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,7 +1,18 @@
+// essential variables
 var startQuiz = document.getElementById("startQuiz")
 var saveScore = document.getElementById("saveScore")
 var viewScores = document.getElementById("viewScores")
+var playAgain = document.getElementById("playAgain")
+
+var welcome = document.getElementById("welcome")
+var quiz = document.getElementById("quiz")
+
+var result = document.getElementById("result")
+
 var timer = document.getElementById("timer")
+
+var options = document.getElementById("options")
+var message = document.getElementById("message")
 
 var summary = document.getElementById("summary")
 
@@ -11,12 +22,41 @@ var score = 0;
 var currentQuestion = 0;
 var countdownTimer;
 
+function onStartQuiz() {
+    secondsLeft = 60;
+    currentQuestion = 0;
+    score = 0;
 
+    quiz.style.display = 'block'
+    startQuiz.style.display = 'none'
+    result.style.display = 'none'
 
-function onStartGame()
+    countdownTimer = setInterval(function () {
+
+        console.log(secondsLeft)
+        if (secondsLeft > 0) {
+            timer.textContent = secondsLeft
+        } else {
+            stopgame();
+        }
+        secondsLeft--;
+    }, 1000);
+
+    displayQuestion(currentQuestion);
+
+function stopgame() {
+    clearInterval(countdownTimer);
+    timer.textContent = "60"
+
+    quiz.style.display = 'none';
+    result.style.display = 'flex';
+
+    var summaryDisplay = document.createElement("p");
+    summary.textContent = "Your Score is: " + score;
+}
+
 function onSaveScore()
 function onViewScores()
-function onStartAgain()
 
 
 
@@ -24,7 +64,7 @@ function onStartAgain()
 
 
 
-startQuiz.addEventListener("click", onStartGame);
+startQuiz.addEventListener("click", onStartQuiz);
 saveScore.addEventListener("click", onSaveScore);
 viewScores.addEventListener("click", onViewScores);
-playAgain.addEventListener("click", onStartGame);
+playAgain.addEventListener("click", onStartQuiz);
